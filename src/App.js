2a4a9008789ca3dd393901b81276700d37180541
NisanurKorkmaz <nisanur29@live.fr>
2023-12-06 20:37:15 +0300

    add component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import Navigation from "./Navigation/Nav";
+import Products from "./Products/Products";
+import Recommended from "./Recommended/Recommended";
+import products from "./db/data";
+
 import "./index";
 
 function App() {
   return (
+    <>
     < Navigation />
+    < Products />
+    < Recommended />
+    </>
+
   )
 }
 export default App;
