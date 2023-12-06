fc9fa7a01cbb8b3433758d5fd2cfe0f46fb5fa9e
NisanurKorkmaz <nisanur29@live.fr>
2023-12-06 19:46:59 +0300

    update navbar

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import Navigation from "./Navigation/Nav";
+import "./index";
+
 function App() {
   return (
     < Navigation />
--- src/Navigation/Nav.js
+++ src/Navigation/Nav.js
@@ -3,16 +3,24 @@ import { AiOutlineShoppingCart, AiOutlineUserAdd } from "react-icons/ai";
 import "./Nav.css";
 
 function Nav() {
-  return 
+    return(
     <nav>
         <div className='nav-container'>
             <input type="text" className="search-input" placeholder='Enter your search' />
         </div>
         <div className='profil-container'>
-            <a href="#"> </a>
+            <a href="#">
+                <FiHeart className="nav-icons" />
+            </a>
+            <a href="">
+                <AiOutlineShoppingCart className="nav-icons" />
+            </a>
+            <a href="">
+                <AiOutlineUserAdd className="nav-icons" />
+            </a>
         </div>
     </nav>
-  
+)
 }
 
 export default Nav
